f15f00c3ee82a69d6f7c0b69a991ad616ca1fbc5
Adam Butt <adambutt0403@gmail.com>
2022-03-14 23:19:41 +0000

    Cleaned up code by removing uneccessary global variables. Changed FILE_WRITE to O_CREAT | O_WRITE and added flush after 512 bytes to see if better write speeds.

--- WirelessDisplay/WirelessDisplay.ino
+++ WirelessDisplay/WirelessDisplay.ino
@@ -15,12 +15,7 @@
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, true);
 
-const int chipSelect = 53; // Define cs pin for sd card
-char z;
-int i, j, x, y, u;
-byte r, g, b, t, SLIDE_TIME=0, IMAGE_COUNT=2;
-
-File image, imgdata;
+byte SLIDE_TIME = 0, IMAGE_COUNT = 2;
 
 void setup() {
   Serial.begin(9600); // Open serial communications and wait for port to open
@@ -29,7 +24,8 @@ void setup() {
   }
 
   Serial.print("Initializing SD card...");
-
+  
+  const int chipSelect = 53; // Define cs pin for sd card
   if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed!"); // Check to see if SD is recognised
     while (1);
@@ -40,20 +36,18 @@ void setup() {
   Serial1.begin(38400);
 
   matrix.begin(); // Start the LED display
-
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   while(Serial1.available() < 1) {
     float time1 = micros();
-    for (x=1; x<=IMAGE_COUNT; x++) { // Iterate for all images on sd card
+    for (int x=1; x<=IMAGE_COUNT; x++) { // Iterate for all images on sd card
       if (Serial1.available() > 0) {
         break;
       }
       String fname = String(x) + ".txt"; // Form image file name
-      image = SD.open(fname); //open image file for reading
+      File image = SD.open(fname); //open image file for reading
   
       for(byte rows=0; rows<16; rows++) {
         byte buffers[192];
@@ -77,82 +71,19 @@ void loop() {
       delay(SLIDE_TIME*1000); // how long each image displays for
     }
     float time2 = micros();
-    float fps = (7.0/(time2-time1))*1000000.0;
+    float fps = (IMAGE_COUNT/(time2-time1))*1000000.0;
     //Serial.println(fps);
   }
 
   readBluetooth();
-  
-//  imgdata = SD.open("imgdata.txt"); // Open image counter file to read how many images are on SD card
-//  z = imgdata.read();
-//  t = imgdata.read();
-//  imgdata.close();
-//  
-//  y = z - '0';
-//  
-//  if (t >= 'A') { // If value is a hex letter, convert to corresponding number
-//    t = t - 55;        
-//  }
-//
-//  else {
-//    t = t - 48;
-//  }
-//
-//  u = t*1000;
-  
-
-//  for(byte rows=0; rows<32; rows++) {
-//    byte buffers[96];
-//    byte counter = 0;
-//    image.read(buffers, sizeof(buffers));
-//    
-//    for(byte column=0; column<32; column++) {
-//        matrix.drawPixel(column, rows, matrix.Color444(hexCheck(buffers[counter]), hexCheck(buffers[counter+1]), hexCheck(buffers[counter+2]))); // Draw the RGB pixel
-//        counter+=3;
-//    }
-//  }
-
-//     for(i=0; i<32; i++) { // Iterate for 32x32 pixels
-//      for(j=0; j<32; j++) {
-//        r = image.read(); // Read values for RGB
-//        g = image.read();
-//        b = image.read();
-//  
-//        if (r >= 'A') { // If value is a hex letter, convert to corresponding number
-//          r = r - 55;        
-//        }
-//
-//        else {
-//          r = r - 48;
-//        }
-//  
-//        if (g >= 'A') {
-//          g = g - 55;  
-//        }
-//
-//        else {
-//          g = g - 48;
-//        }
-//  
-//        if (b >= 'A') {
-//          b = b - 55;  
-//        }
-//
-//        else {
-//          b = b - 48;
-//        }
-//        
-//        matrix.drawPixel(j, i, matrix.Color444(r, g, b)); // Draw the RGB pixel
-//      }
-//    }
 }
 
 void readBluetooth() {
+  int bytesize = 64;
+  int chunksize = 3072/bytesize;
+
   IMAGE_COUNT = Serial1.read();
   SLIDE_TIME = Serial1.read();
-
-  Serial.print(IMAGE_COUNT);
-  Serial.print(SLIDE_TIME);
 
   for(int count=1; count<=IMAGE_COUNT; count++) {
     String filename = String(count) + ".txt"; // Form image file name
@@ -161,20 +92,23 @@ void readBluetooth() {
       SD.remove(filename);
     }
     
-    File saveBluetooth = SD.open(filename, FILE_WRITE); //open image file for writing
-
-    for(byte chunks=0; chunks<48; chunks++) {
-      byte btbuffer[64];
-      for(byte single_byte=0; single_byte<64; single_byte++) {
+    File saveBluetooth = SD.open(filename, O_CREAT | O_WRITE); //open image file for writing
+    
+    for(byte chunks=0; chunks<chunksize; chunks++) {
+      byte btbuffer[bytesize];
+      for(byte single_byte=0; single_byte<bytesize; single_byte++) {
         //Serial.write( Serial1.read() );
         btbuffer[single_byte] = Serial1.read();
-        //saveBluetooth.write(Serial1.read());
       }
-      saveBluetooth.write(btbuffer, 64);
+      saveBluetooth.write(btbuffer, bytesize);
       Serial.println(chunks);
+      
+      if(chunks % 512/bytesize == 0) { // When 512 bytes are written to the file, copy the data physically to the SD card using flush()
+        saveBluetooth.flush();
+      }
+      
       delay(300);
     }
-
     saveBluetooth.close();
     Serial.println("Upload done");
   }
